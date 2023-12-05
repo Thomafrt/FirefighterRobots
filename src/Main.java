4f8afef1f968ffcdf5ec555ba45c0ffd9bf639c5
Ignitfire <84183590+Ignitfire@users.noreply.github.com>
2023-12-05 20:08:36 +0100

    modified main

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
 					int sleepTime = 100; //Le temps d'attente entre chaque tour en ms
 					double propagationProb = 0.5; //Pourcentage de chance de propagation d'une case à sa voisine
 					double extinctionProb = 0.3; //Pourcentage de chance que le feu consume entièrement la case
-					int gridSize = 25; //Taille de la grille
+					int gridSize = 40; //Taille de la grille
 					int nbHumans = 20; //Nombre d'humains sur la grille (placés aléatoirement)
 					int nbFireStart = 3; //Nombre de départs de feu (placés aléatoirement)
                     ////////////////////////////////////////////////////////////////////////////////////////////
