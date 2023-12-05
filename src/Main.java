356ad5158e4f1944dff40f29be95556e07441494
Thomas <thom.foret@free.fr>
2023-12-05 19:25:17 +0100

    modales choix paramètres

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,39 @@
 import java.awt.EventQueue;
+import javax.swing.JOptionPane;
 
 public class Main {
-    	/**
-	 * Launch the application.
-	 */
+    /**
+     * Launch the application.
+     */
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
+                    // Valeurs par défaut
+                    int defaultNbRobots = 25;
+                    int defaultSleepTime = 100;
+                    double defaultPropagationProb = 0.5;
+                    double defaultExtinctionProb = 0.3;
+                    int defaultGridSize = 25;
+                    int defaultNbHumans = 20;
+                    int defaultNbFireStart = 3;
 
-                    /////////////////////////////////////////////////////////////////////////////////////////////
-					//PARAMETRES modifiables
-					int nbRobots = 25; //Le nombre de robots
-					int sleepTime = 100; //Le temps d'attente entre chaque tour en ms
-					double propagationProb = 0.5; //Pourcentage de chance de propagation d'une case à sa voisine
-					double extinctionProb = 0.3; //Pourcentage de chance que le feu consume entièrement la case
-					int gridSize = 25; //Taille de la grille
-					int nbHumans = 20; //Nombre d'humains sur la grille (placés aléatoirement)
-					int nbFireStart = 3; //Nombre de départs de feu (placés aléatoirement)
-                    ////////////////////////////////////////////////////////////////////////////////////////////
+                    // Demander à l'utilisateur de saisir les paramètres avec des valeurs par défaut
+                    int nbRobots = Integer.parseInt(JOptionPane.showInputDialog(null, "Nombre de robots:", defaultNbRobots));
+                    int sleepTime = Integer.parseInt(JOptionPane.showInputDialog(null, "Temps d'attente entre chaque tour (ms):", defaultSleepTime));
+                    double propagationProb = Double.parseDouble(JOptionPane.showInputDialog(null, "Probabilité de chance de propagation du feu:", defaultPropagationProb));
+                    double extinctionProb = Double.parseDouble(JOptionPane.showInputDialog(null, "Probabilité de chance d'extinction du feu:", defaultExtinctionProb));
+                    int gridSize = Integer.parseInt(JOptionPane.showInputDialog(null, "Taille de la grille:", defaultGridSize));
+                    int nbHumans = Integer.parseInt(JOptionPane.showInputDialog(null, "Nombre d'humains sur la grille:", defaultNbHumans));
+                    int nbFireStart = Integer.parseInt(JOptionPane.showInputDialog(null, "Nombre de départs de feu:", defaultNbFireStart));
 
-					//Créer la grille
+                    // Créer la grille
                     GridView frame = new GridView(nbRobots, gridSize, propagationProb, nbHumans, nbFireStart);  // Passer la taille souhaitée
                     frame.setVisible(true);
-					//Lancer les tours
-					Turn turn = new Turn(frame, sleepTime, propagationProb, extinctionProb);
-					turn.run();
+
+                    // Lancer les tours
+                    Turn turn = new Turn(frame, sleepTime, propagationProb, extinctionProb);
+                    turn.run();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
